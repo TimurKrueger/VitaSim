42e322c00a085d59bdb89cc53a7cc3a0edb453ee
Timur <timur.krueger@googlemail.com>
2024-08-27 15:58:49 +0200

    Update

---
+++ VitaSim/Assets/Shooter Game/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    private int numOfHearts=(int)PlayerStats.health;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        numOfHearts=(int)PlayerStats.getHealth();
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < numOfHearts)
+            {
+                hearts[i].sprite=fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite=emptyHeart;
+            }
+        }
+    }
+}
